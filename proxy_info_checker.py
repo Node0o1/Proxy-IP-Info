a37b191ab3d21772070d4becfb71b8dd22e3ecef
Node <157242958+Node0o1@users.noreply.github.com>
2024-01-21 01:24:49 -0500

    Update proxy_info_checker.py

--- proxy_info_checker.py
+++ proxy_info_checker.py
@@ -19,22 +19,22 @@ BROWSER_HEADERS = [
 
 def get_pub_ip():
     try:
-        content=json.loads(requests.get('https://api.myip.com', timeout=TIMEOUT).text)
+        content = json.loads(requests.get('https://api.myip.com', timeout= TIMEOUT).text)
     except:
         return 'Public IP currently unavailable.'
     else:
         return content['ip']
 
 def check(proxy):
-    rand_header = BROWSER_HEADERS[random.randint(0, len(BROWSER_HEADERS)-1 )]
-    proxies={'https': proxy}
+    rand_header = BROWSER_HEADERS[random.randint(0, len(BROWSER_HEADERS) -1 )]
+    proxies = {'https': proxy}
     try:
-        content=requests.get('https://api.myip.com', proxies=proxies,headers=rand_header, timeout= TIMEOUT, verify= True)
+        content = requests.get('https://api.myip.com', proxies= proxies, headers= rand_header, timeout= TIMEOUT, verify= True)
     except Exception as e:
         None
     else:
         try:
-            prox_details=json.loads(content.text)
+            prox_details = json.loads(content.text)
         except:
             print(f'{chr(0x0a)}Error encountered with {proxy}, skipping.')
         else:
