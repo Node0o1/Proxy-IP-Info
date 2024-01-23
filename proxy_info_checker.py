d8eb69b12cd5626af9a49582b313ed6f35513270
Node001 <157242958+Node0o1@users.noreply.github.com>
2024-01-22 19:38:06 -0500

    Update proxy_info_checker.py

--- proxy_info_checker.py
+++ proxy_info_checker.py
@@ -41,15 +41,15 @@ def check_list(in_file= None):
         if(in_file == None):
             in_file=str(input("ENTER PROXY FILE PATH/NAME: "))
         print(f'YOUR PUBLIC IP: {get_pub_ip()}')
-        with open(in_file, mode='r') as fhandle:
-            pThread().map(check,['http://'+ str(prx).strip() for prx in fhandle.read().split()]) 
+        try:
+            with open(in_file, mode='r') as fhandle:
+                pThread().map(check,['http://'+ str(prx).strip() for prx in fhandle.read().split()]) 
+        except FileExistsError as e:
+            print(f'{type(e).__name__} ERROR :: {e.args}')
     except Exception as e:
         print(f'{type(e).__name__} ERROR :: {e.args}')
 
-
 if __name__=="__main__":
-    if(len(sys.argv) > 1):
-        infile = sys.argv[1]
-    else:
-        infile = None
+    if(len(sys.argv) > 1): infile = sys.argv[1]
+    else: infile = None
     check_list(infile)
