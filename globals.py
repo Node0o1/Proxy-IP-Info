d473ca5b7b31c2c4c72115a525230baae74feddd
Node001 <157242958+Node0o1@users.noreply.github.com>
2024-02-20 17:47:13 -0500

    Update globals.py

--- globals.py
+++ globals.py
@@ -1,6 +1,7 @@
 PROCESSES=10
 MAX_TASK=4
 TIMEOUT = 20
+
 BROWSER_HEADERS = [
     {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0"},#FireFox
     {"User-Agent":"Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136"},#Edge
